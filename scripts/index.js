3ec5306b8e138e45ea6c30623dfc6d351563da45
Natalia Pushkina <pshknnt@yandex.ru>
2022-03-30 19:11:57 +0400

    bugfix

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,11 @@
-const editBtn = document.querySelector('.edit-button');
+const editBtn = document.querySelector('.button_edit');
 const popup = document.querySelector('.popup');
-const closePopupBtn = document.querySelector('.popup__close');
+const closePopupBtn = document.querySelector('.button_close');
 
 function togglePopUp() {
   popup.classList.toggle('popup_opened');
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileAbout.textContent;
 }
 
 editBtn.addEventListener('click', togglePopUp);
@@ -13,18 +15,14 @@ closePopupBtn.addEventListener('click', togglePopUp);
 const formElement = document.querySelector('.popup__form');
 const profileName = document.querySelector('.profile__title');
 const profileAbout = document.querySelector('.profile__subtitle');
-const nameInput = document.querySelector('.popup__form-name');
-const jobInput = document.querySelector('.popup__form-about');
+const nameInput = document.querySelector('.input__item_name');
+const jobInput = document.querySelector('.input__item_job');
 
-
-nameInput.value = profileName.textContent;
-jobInput.value = profileAbout.textContent;
 
 function formSubmitHandler(evt) {
   evt.preventDefault();
   profileName.textContent = nameInput.value;
   profileAbout.textContent = jobInput.value;
-
   togglePopUp();
 }
 
