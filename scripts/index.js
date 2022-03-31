3f41564a584486e361e1f0ab6124feb82e48a035
Natalia Pushkina <pshknnt@yandex.ru>
2022-03-31 23:14:55 +0400

    bugfix html, rename classes

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,6 @@
-const editBtn = document.querySelector('.button_edit');
+const editBtn = document.querySelector('.button_type_edit');
 const popup = document.querySelector('.popup');
-const closePopupBtn = document.querySelector('.button_close');
+const closePopupBtn = document.querySelector('.button_type_close');
 
 function togglePopUp() {
   popup.classList.toggle('popup_opened');
@@ -12,11 +12,11 @@ editBtn.addEventListener('click', togglePopUp);
 
 closePopupBtn.addEventListener('click', togglePopUp);
 
-const formElement = document.querySelector('.form');
+const formElement = document.querySelector('.popup__form');
 const profileName = document.querySelector('.profile__title');
 const profileAbout = document.querySelector('.profile__subtitle');
-const nameInput = document.querySelector('.form__item_name');
-const jobInput = document.querySelector('.form__item_job');
+const nameInput = document.querySelector('.popup__input_el_name');
+const jobInput = document.querySelector('.popup__input_el_job');
 
 function formSubmitHandler(evt) {
   evt.preventDefault();
